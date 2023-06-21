f09bf814434fa9c45c39f6ce8bd0c5eedb4e1d92
Rahul Rajput <rr611305@gmail.com>
2023-06-21 12:55:08 +0530

    checking if its deploying

--- src/app/genre/[genre]/page.tsx
+++ src/app/genre/[genre]/page.tsx
@@ -1,25 +1,27 @@
 "use client";
 import LoadingPage from "../../../components/LoadingPage";
 import { useQuery } from "react-query";
-// import { META } from "@consumet/extensions";
+import { META } from "@consumet/extensions";
 
 const Anime = ({ params }: any) => {
-  // const { genre } = params;
+  const { genre } = params;
+  console.log(genre);
+  const genreArr = []
+  genreArr.push(genre)
+  const fetchAnimeByGenre = async (genre:any) => {
+    const Anilist = new META.Anilist();
+    const results = await Anilist.fetchAnimeGenres(genre);
+    console.log(results);
+    return results;
+  };
+  const { data, isError, isLoading, status } = useQuery({
+    queryKey: ["genreQuery", genreArr],
+    queryFn: () => fetchAnimeByGenre(genre),
+  });
+  console.log(data);
 
-  // const fetchAnimeByGenre = async (type: string) => {
-  //   const Anilist = new META.Anilist();
-  //   const results = await Anilist.fetchAnimeGenres([type]);
-  //   console.log(results);
-  //   return results;
-  // };
-  // const { data, isError, isLoading, status } = useQuery({
-  //   queryKey: ["genreQuery", genre],
-  //   queryFn: () => fetchAnimeByGenre(genre),
-  // });
-  // console.log(data);
-  
-  // if (isLoading) return <LoadingPage />;
-  // if (isError) return <h1>Error loading data!!!</h1>;
+  if (isLoading) return <LoadingPage />;
+  if (isError) return <h1>Error loading data!!!</h1>;
 
   return <></>;
 };
