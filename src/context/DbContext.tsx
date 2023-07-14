b335710fa791863680eca20be290c0a9edb5dd13
Rahul Rajput <rr611305@gmail.com>
2023-07-14 11:49:28 +0530

    context error

--- src/context/DbContext.tsx
+++ src/context/DbContext.tsx
@@ -1,11 +1,6 @@
 "use client";
 import { createContext, useContext } from "react";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
-import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "../firebase";
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 const DbContext = createContext<any>(null);
 
@@ -14,14 +9,6 @@ export function useAuth() {
 }
 
 export function DbProvider({ children }: any) {
-  const addToWatchlist = async (collection: any, data: any) => {
-    try {
-      const docRef = await addDoc(collection(db, collection), data);
-      console.log("Document written with ID: ", docRef);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  };
-  
-  return <DbContext.Provider value={{addToWatchlist}}>{children}</DbContext.Provider>;
+
+  return <DbContext.Provider value={{}}>{children}</DbContext.Provider>;
 }
